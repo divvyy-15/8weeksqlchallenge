95d5f3d455a922d09cd6fdd0b179648a87858a79
divvyy-15 <164885295+divvyy-15@users.noreply.github.com>
2024-03-26 01:37:28 +0530

    Create Q6_soln.sql
    Q6 solution added

---
+++ Case Study #1 - Danny's Diner/Q6_soln.sql
@@ -0,0 +1,12 @@
+--6)Which item was purchased first by the customer after they became a member?
+with cte as
+(select s.customer_id as customer_id,s.product_id,m.join_date,s.order_date,
+rank() over(partition by s.customer_id order by order_date) as rn from sales s
+inner join members m
+on s.customer_id = m.customer_id
+where s.order_date > m.join_date)
+
+select c.customer_id,mu.product_name from cte c
+inner join menu mu
+on c.product_id = mu.product_id
+where rn = 1
