2347a89c3a2123042d5e9787fc968274b4bc0b47
divvyy-15 <164885295+divvyy-15@users.noreply.github.com>
2024-03-29 15:58:14 +0530

    Update runner.sql
    added more solutions

--- Case Study #2 - Pizza Runner/B - Runner and Customer Experience/runner.sql
+++ Case Study #2 - Pizza Runner/B - Runner and Customer Experience/runner.sql
@@ -10,9 +10,20 @@ with cte as
 select runner_id,cast(round(avg(time_diff),2) as decimal(10,2)) as avg_time from cte
 group by runner_id
 
+--3. Is there any relationship between the number of pizzas and how long the order takes to prepare?
+with cte_prep_time as
+(select order_id,count(order_id) as pizza_count,DATEDIFF(minute,order_time,pickup_time) as prep_time from delivered_orders
+group by order_id,order_time,pickup_time)
+select pizza_count,avg(prep_time) as avg_prep_time_required from cte_prep_time
+group by pizza_count
 
+--4. What was the average distance travelled for each customer?
+select customer_id,cast(avg(distance)as decimal(10,2)) as avg_distance_travelled from delivered_orders
+group by customer_id
+order by 2 desc
 
-
+--5. What was the difference between the longest and shortest delivery times for all orders?
+select max(duration)-min(duration) as diff_in_longest_shortest_del_time from delivered_orders
 
 --6. What was the average speed for each runner for each delivery and do you notice any trend for these values?
 SELECT order_id,runner_id,
@@ -20,3 +31,5 @@ CAST(AVG(distance / (duration * 1.00 / 60)) AS decimal(10, 2)) AS avg_speed_kmph
 FROM delivered_orders
 GROUP BY order_id,runner_id
 ORDER BY runner_id;
+
+
